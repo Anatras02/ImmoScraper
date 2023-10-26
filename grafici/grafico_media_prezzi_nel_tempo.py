34889c7e7d2fb6b1b0ed25997252390aa255fe6a
Alessio Balderi <ingress1234ab@gmail.com>
2023-10-26 09:37:53 +0200

    Sorted values in grafico_media_prezzi_nel_tempo

--- grafici/grafico_media_prezzi_nel_tempo.py
+++ grafici/grafico_media_prezzi_nel_tempo.py
@@ -3,8 +3,10 @@ from matplotlib import pyplot as plt
 
 def plot_grafico_media_prezzi_nel_tempo(annunci):
     annunci_senza_prezzi_nan = annunci.dropna(subset=["prezzo"], inplace=False).copy()
+    annunci_senza_prezzi_nan.sort_values(by="data_ultima_modifica_prezzo", inplace=True)
     annunci_senza_prezzi_nan["media_settimanale"] = annunci_senza_prezzi_nan["prezzo"].rolling(window=7,
                                                                                                min_periods=1).mean()
     annunci_senza_prezzi_nan["media_settimanale"].interpolate(method="linear", inplace=True)
     annunci_senza_prezzi_nan.plot(x='data_ultima_modifica_prezzo', y='media_settimanale')
     plt.show()
+
