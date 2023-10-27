2f78c77d4d7d7fa75b808d1a3243b3de1944a076
Alessio Balderi <ingress1234ab@gmail.com>
2023-10-27 19:12:12 +0200

    Price/sq.m chart removes categories when there's not enough data.

--- grafici/grafico_media_prezzi_nel_tempo_per_categoria.py
+++ grafici/grafico_media_prezzi_nel_tempo_per_categoria.py
@@ -1,5 +1,4 @@
 from matplotlib import pyplot as plt
-import pandas as pd
 
 
 # Funzione per preparare i dati
@@ -8,6 +7,8 @@ def prepara_dati(annunci):
     annunci_preparati.sort_values(by="data_ultima_modifica_prezzo", inplace=True)
     annunci_preparati["media_settimanale"] = annunci_preparati["prezzo"].rolling(window=7, min_periods=1).mean()
     annunci_preparati["media_settimanale"].interpolate(method="linear", inplace=True)
+    annunci_preparati = annunci_preparati[annunci_preparati["media_settimanale"] > 0]
+
     return annunci_preparati
 
 
@@ -24,10 +25,17 @@ def plot_categoria(ax, dati_categoria, nome_categoria):
     ax.legend()
 
 
+def _get_categorie_con_abbastanza_dati(categorie, dati_preparati, soglia=5):
+    return [
+        categoria for categoria in categorie if
+        len(dati_preparati[dati_preparati['nome_tipologia'] == categoria]) > soglia
+    ]
+
+
 # Funzione principale per plottare il grafico per tutte le categorie
 def plot_grafico_media_prezzi_nel_tempo_per_categoria(annunci):
     dati_preparati = prepara_dati(annunci)
-    categorie = dati_preparati['nome_tipologia'].unique()
+    categorie = _get_categorie_con_abbastanza_dati(dati_preparati['nome_tipologia'].unique(), dati_preparati)
 
     fig, axs = plt.subplots(len(categorie), 1, figsize=(10, 3 * len(categorie)))
 
